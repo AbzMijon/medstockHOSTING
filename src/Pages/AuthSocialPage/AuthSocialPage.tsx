133be58baa08438ee38042f3796d181bd664a9cd
AbzMijon <egorwebdev8@gmail.com>
2024-05-15 15:56:32 +0300

    change route

--- src/Pages/AuthSocialPage/AuthSocialPage.tsx
+++ src/Pages/AuthSocialPage/AuthSocialPage.tsx
@@ -9,6 +9,9 @@ import { loginWithOAuthKeyThunk } from '@src/api/authApi/authThunks';
 const AuthSocialPage = () => {
     const dispatch = useAppDispatch();
 
+    console.log('localhost:3000/key_auth/?key=28319d04-e303-47a2-9815-1e44c0612616&success=true');
+    
+
     useEffect(() => {
         const params = parseUriParams(window.location.href);
         console.log('params', params);
